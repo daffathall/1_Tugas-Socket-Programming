ab2e533d2f56e811e2c6b51edb1d8dfcc865caff
daffathall <165241712+daffathall@users.noreply.github.com>
2024-11-01 17:06:40 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,51 @@
+import socket
+import argparse
+import threading
+import os
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.host, self.port))
+        self.clients = {}  # Ngestore clients, addresses, dan usernames
+        print(f"Server is listening at {self.host}:{self.port}")
+
+
+    def start(self):
+        print("Waiting for messages...")
+        while True:
+            try:
+                data, addr = self.sock.recvfrom(1024)
+                message = data.decode('ascii')
+
+                if message.startswith('LOGIN'):
+                    _, username, password = message.split(maxsplit=2)
+                    if username in self.clients.values():
+                        self.sock.sendto('Username sudah dipakai. Gunakan yang lain.'.encode('ascii'), addr)
+                    elif password != 'GAS1':
+                        self.sock.sendto('Password salah. Silahkan coba lagi.'.encode('ascii'), addr)
+                    else:
+                        self.clients[addr] = username
+                        self.sock.sendto(f'Welcome {username}!'.encode('ascii'), addr)
+                        print(f'{username} sudah bergabung {addr}.')
+                else:
+                    username = self.clients.get(addr, 'Unknown')
+                    print(f'Received message from {username} at {addr}: {message}')
+                    self.broadcast(message, addr)
+            except Exception as e:
+                print(f'error: {e}')
+
+    def broadcast(self, message, sender_addr):
+        for addr in self.clients:
+            if addr != sender_addr:
+                self.sock.sendto(message.encode('ascii'), addr)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="UDP Chatroom Server")
+    parser.add_argument('host', help='The host IP address')
+    parser.add_argument('-p', metavar='PORT', type=int, default=8080, help='Port number')
+    args = parser.parse_args()
+    server = Server(args.host, args.p)
+    server.start()
